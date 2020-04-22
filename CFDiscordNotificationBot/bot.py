ee30a902d9f31083afb642ef1f626b5c20f7bcfa
Sameh Amnoun <samehmamnoun@gmail.com>
2020-04-23 00:18:17 +0200

    feat : Added command ;upcoming
    it shows all upcoming contests in an embedded msg

--- CFDiscordNotificationBot/bot.py
+++ CFDiscordNotificationBot/bot.py
@@ -2,6 +2,9 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
+import CFDiscordNotificationBot.CFAPI
+import datetime
+
 load_dotenv()
 SECRET_KEY = os.getenv("DISCORD_TOKEN")
 
@@ -16,5 +19,23 @@ async def on_ready():
 @client.command()
 async def hi(ctx):
     await ctx.send("Hello World")
+
+
+@client.command()
+async def upcoming(ctx):
+    contestsData = discord.Embed(
+        title="Upcoming Codeforces Rounds",
+        color=discord.Colour.dark_blue()
+    )
+    contests = CFDiscordNotificationBot.CFAPI.getBeforeContests()
+    data = ''
+    for contest in contests[::-1]:
+        data += (f"**{contest.name}**"
+                 f"\n\u200bStart Time: "
+                 f"{datetime.datetime.fromtimestamp(contest.startTimeSeconds).strftime('%Y-%m-%d %H:%M:%S')}"
+                 f"\n\u200bDuration Time: {contest.durationSeconds / 60 / 60} hours\n\n")
+    contestsData.add_field(name='\u200b', value=data)
+    await ctx.send(embed=contestsData)
+
 
 client.run(SECRET_KEY)

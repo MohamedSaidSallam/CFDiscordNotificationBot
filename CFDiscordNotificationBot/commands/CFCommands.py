1906e90a8c7eab01c6d816f93395c406d78d7abe
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2020-04-30 15:26:48 +0200

    feat: Before start (upcoming)

--- CFDiscordNotificationBot/commands/CFCommands.py
+++ CFDiscordNotificationBot/commands/CFCommands.py
@@ -8,6 +8,20 @@ import CFDiscordNotificationBot.CFAPI
 CF_LOGO = "https://sta.codeforces.com/s/14049/images/codeforces-telegram-square.png"
 
 
+def getFormattedBeforeStart(relativeTimeSeconds):
+    beforeStart = -1 * relativeTimeSeconds
+    beforeStartPostfix = "sec(s)"
+    if beforeStart > 60:
+        beforeStartPostfix = "min(s)"
+        beforeStart /= 60
+        if beforeStart > 60:
+            beforeStartPostfix = "hr(s)"
+            beforeStart /= 60
+            if beforeStart > 24:
+                beforeStartPostfix = "day(s)"
+                beforeStart /= 24
+    return beforeStart, beforeStartPostfix
+
 class CF(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -21,11 +35,14 @@ class CF(commands.Cog):
         )
         contests = CFDiscordNotificationBot.CFAPI.getBeforeContests()
         for contest in contests[::-1]:
+            beforeStart, beforeStartPostfix = getFormattedBeforeStart(
+                contest.relativeTimeSeconds)
             contestsData.add_field(
                 name=f"**{contest.name}**",
-                value=f"@_{datetime.fromtimestamp(contest.startTimeSeconds).strftime('%m-%d %H:%M')}_\n"
-                f"Duration: _{contest.durationSeconds / 60 / 60}_ hrs\n"
-                f"Scoring System: _{contest.type}_",
+                value=f"@_{datetime.fromtimestamp(contest.startTimeSeconds).strftime('%m-%d %H:%M')}_"
+                f", In _{int(beforeStart)}_ {beforeStartPostfix}\n"
+                f"Duration: _{contest.durationSeconds / 60 / 60}_ hr(s)\n"
+                f"Scoring System: _{contest.type}_\n",
                 inline=False)
         contestsData.set_thumbnail(url=CF_LOGO)
         await ctx.send(embed=contestsData)

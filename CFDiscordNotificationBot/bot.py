d8162ccf837688037f58b3802e8398acc06c0367
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2020-05-02 00:20:10 +0200

    feat: Bot Status ";help"

--- CFDiscordNotificationBot/bot.py
+++ CFDiscordNotificationBot/bot.py
@@ -13,6 +13,7 @@ client = commands.Bot(command_prefix=';')
 @client.event
 async def on_ready():
     print('Bot is ready')
+    await client.change_presence(activity=discord.Game(name=';help'))
 
 
 @client.command()
